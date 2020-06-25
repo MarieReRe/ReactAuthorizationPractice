d61a5596ea235b402c9d61905d4fc711593ebbc2
Marie Antons <chefantons@gmail.com>
2020-06-25 18:15:46 -0500

    Nav complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Route } from "react-router-dom";
 import Home from './home/Home';
 import Profile from './profile/profile'
+import Nav from './nav'
 
 export default class App extends Component {
   render() {
 
     return (
       <>
+      <Nav />
         <Route path="/" exact component={Home} />
         <Route path="/profile" exact component={Profile} />
       </>
