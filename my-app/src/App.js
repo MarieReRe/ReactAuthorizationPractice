e441d2aa3eada8c53af80bfa218d7a2566bb98f3
Marie Antons <chefantons@gmail.com>
2020-06-25 18:10:15 -0500

    setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route } from "react-router-dom";
+import Home from './home/Home';
+import Profile from './profile/profile'
 
+export default class App extends Component {
+  render() {
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+    return (
+      <>
+        <Route path="/" exact component={Home} />
+        <Route path="/profile" exact component={Profile} />
+      </>
+    )
+
+  };
 }
 
-export default App;
+
