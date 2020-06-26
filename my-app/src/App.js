0cad2d373e10e508120adca01da6e941ec20e1b0
Marie Antons <chefantons@gmail.com>
2020-06-25 19:36:44 -0500

    added history injection

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from "react-router-dom";
 import Home from './home/Home';
 import Profile from './profile/profile'
 import Nav from './nav'
 import './index.css'
+import Auth from './Auth/Auth';
 
-export default class App extends Component {
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.auth = new Auth(this.props.history);
+  }
   render() {
 
     return (
